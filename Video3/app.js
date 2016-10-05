5cdb6015726113a1bc6f73d839dc97d3fd9239ef
deepakchampatiray <deepakchampatiray@gmail.com>
2016-10-05 01:49:22 -0400

    Completed Video 3

---
+++ Video3/app.js
@@ -0,0 +1,7 @@
+var myApp = angular.module('myModule', []).controller('myController', function($scope){
+  $scope.employee = {
+    firstName: 'Deepak',
+    lastName: 'Champatiray',
+    company: 'TCS'
+  }
+});
